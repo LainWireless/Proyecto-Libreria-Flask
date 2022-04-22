4683a17bfc0838325ec9bf8efa9c1a06b1a45a36
Ivan <ivanpicas88@gmail.com>
2022-04-22 15:07:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,10 @@ datos = json.load(f)
 def inicio():
 	return render_template("inicio.html",lista_libros=datos)
 
+@app.route('/libro/<isbn>')
+def libros(isbn):
+    for libro in datos:
+        if libro.get("isbn")==isbn:
+            return render_template("libro.html",contenido=libro)
+    abort(404)
 app.run('0.0.0.0',5000,debug=True)
