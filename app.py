169ffe0f19407b2aba339882c3686a634e2bc4df
Ivan <ivanpicas88@gmail.com>
2022-04-22 15:04:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ f = open('Libreria/books.json')
 
 datos = json.load(f)
 
+@app.route('/')
+def inicio():
+	return render_template("inicio.html",lista_libros=datos)
+
 app.run('0.0.0.0',5000,debug=True)
