85b53d369c09a7b6bb1c8479dbd11f15226bb252
Ivan <ivanpicas88@gmail.com>
2022-04-22 15:08:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,14 @@ def libros(isbn):
         if libro.get("isbn")==isbn:
             return render_template("libro.html",contenido=libro)
     abort(404)
+    
+@app.route('/categoria/<tipo>')
+def categoria(tipo):
+    listacategorias=[]
+    for cate in datos:
+        for lista in cate.get("categories"):
+            if lista==tipo:
+                listacategorias.append(cate)
+                return render_template("categoria.html",categoria=tipo,lista_categorias=listacategorias,lista_libros=datos)
+
 app.run('0.0.0.0',5000,debug=True)
